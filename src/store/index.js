e049b5e9fb3d40f632c7abc1de9c11cbb64834fd
Oseas <O.a.sanchez@outlook.com>
2020-10-11 15:58:02 -0400

    Obteniendo la Data de la API

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
+    Data: [],
   },
   mutations: {
+
+    SetData(state,payload)
+    {
+      state.Data = payload;
+    },
+
   },
   actions: {
+
+    async GetData({ commit })
+    { 
+        await axios.get('https://my-json-server.typicode.com/OArnaudSanchez/demo/test')
+              .then(datos => {
+                  const {data} = datos;
+                  commit('SetData', data);
+              })
+              .catch(error => console.log(error));
+    }
   },
-  modules: {
-  }
+  getters:{},
+  modules: {}
 })
