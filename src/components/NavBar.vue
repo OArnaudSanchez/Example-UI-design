2e8a046f727b5d4b51a37265dee59a0b71fda9d4
Oseas <O.a.sanchez@outlook.com>
2020-10-11 16:00:39 -0400

    Componente NavBar terminado

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,134 +1,173 @@
 <template>
     <div>
         <div id="content-wrapper" class="d-flex">
-            <div id="sidebar-container" class="bg-light border-right">
-                <nav class="navbar navbar-light bg-white p-3">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#opcionMenu" aria-controls="opcionMenu" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </nav>
-
-                <div class="menu collapse list-group-flush" id="opcionMenu">
-                    <a href="" class="list-group-item list-group-item-action bg-light p-3 border-0">
-                        <img src="@/assets/usersLogo.png" alt="" class="lead mr-2"> <span>Users</span>
-                    </a>
-                    <a href="" class="list-group-item list-group-item-action bg-light p-3 border-0">
-                        <img src="@/assets/cashLogo.png" alt="" class="lead mr-2"> <span>Money</span>
-                    </a>
-                    <a href="" class="list-group-item list-group-item-action bg-light p-3 border-0">
-                        <img src="@/assets/configurationLogo.png" alt="" class="lead mr-2"> <span>Configuration</span>
-                    </a>
-                    <a href="" class="list-group-item list-group-item-action bg-light p-3 border-0">
-                        <img src="@/assets/statsLogo.png" alt="" class="lead mr-2"> <span>Stats</span>
-                    </a>
-                </div>
-            </div>
-
             <div id="pageContainer" class="w-100 bg-light-blue">
-                <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-                    <div class="container">
-                        <span class="badge badge-pill badge-light">
-                        <span class="badgeClass">Cartera</span> / 
-                        <span class="badgeClass">Clientes</span> / 
-                        <strong class="badgeClass">Nombre Cliente</strong>
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
 
-                        </span>
-
+                        <div id="bread-crumbs">
+                            <span class="badge badge-pill badge-light">
+                                <span class="badgeClass">Cartera > </span> 
+                                <span class="badgeClass">Clientes > </span> 
+                                <strong class="badgeClass">Nombre Cliente</strong>
+                            </span>
+                        </div>
+                        
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-
+                            
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav ml-auto">
                                 <li class="nav-item active">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/statsLogo.png" alt="">
+                                        <img src="@/assets/statsLogo.png" class="img" alt="">
                                     </a>
-                                    
                                 </li>
+
+                                <hr id="line-copy">
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/newspaperLogo.png" alt="">
+                                        <img src="@/assets/newspaperLogo.png" class="img" alt="">
                                     </a>
-                                    
                                 </li>
+                                <hr id="line-copy">
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/searchLogo.png" alt="">
+                                        <img src="@/assets/searchLogo.png" class="img" alt="">
                                     </a>
-                                    
                                 </li>
+
+                                <hr id="line-copy">
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/copyLogo.png" alt="">
+                                        <img src="@/assets/copyLogo.png" class="img" alt="">
                                     </a>
-                                    
                                 </li>
+                                <hr id="line-copy">
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/questionLogo.png" alt="">
+                                        <img src="@/assets/questionLogo.png" class="img" alt="">
                                     </a>
-                                    
                                 </li>
+                                <hr id="line-copy">
+
                                 <li class="nav-item">
                                     <a class="nav-link" href="#">
-                                        <img src="@/assets/ovalRedLogo.png" width="10" class="mb-3" alt="">
+                                        <img src="@/assets/ovalRedLogo.png" width="20" class="mb-3 img" alt="">
                                     </a>
                                     
                                 </li>
-                                <li class="nav-item dropdown">
+                                <hr id="line-copy">
                                 
+                                <li class="nav-item dropdown m-2 w-100" >
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img src="@/assets/abelardo.png" alt="">
+                                    <img src="@/assets/abelardo.png" class="mr-1" alt="">
                                         <span>Abelardo</span> 
                                     </a>
-                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Mi Perfil</a>
+                                    <div class="dropdown-menu "  aria-labelledby="navbarDropdown">
+                                        
+                                        <a class="dropdown-item" id="abelardo-acebado" href="#">Abelardo Acebado</a>
+
                                         <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Cerrar Sesion</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
+                                        
+                                        <a class="dropdown-item" href="#">
+                                            <img src="@/assets/iconUser.png" alt="">
+                                            Datos Personales
+                                        </a>
+
+                                        
+                                        <a class="dropdown-item" href="#">
+                                            <img src="@/assets/iconUserConfig.png" alt="">
+                                            Configuracion
+                                        </a>
+                                        
+                                        <a class="dropdown-item text-danger" href="#">
+                                            <img src="@/assets/iconExit.png" alt="">
+                                            Salir de mi Cuenta
+                                        </a>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </nav>
             </div>
-        </div>
+        </div>    
     </div>
     
 </template>
 
 <script>
 export default {
-    name: 'NavBar'
+    name: 'NavBar',
 }
 </script>
-<style>
-    :root{
-        --light: #ffffff;
-    }
-    .bg-light{
-        background-color: var(--light) !important;
+
+<style scoped>
+    .navbar{
+        width: 100%;
+        height: 60px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.05);
+        background-color: #ffffff;
     }
 
-    #sidebar-container{
-        min-height: 100vh; /**Altura del sidebar */
-        /* margin-left: 18rem; *Margen izquierdo */
-        transition: margin, .25s ease-in-out;
-    }
-    #sidebar-container .logo{
-        padding: .875rem 1.25rem;
-        font-size: 1.2rem;
-    }
-    #sidebar-container .menu{
-        width: 12rem; /**Ancho del sidebar */
-    }
-
-    .badgeClass{
+    #bread-crumbs .badge span{
         color: #8492af;
     }
     
-    /* #prueba{
-        color: #9b9b9b;
-    } */
+    #bread-crumbs .badge strong{
+        color: #415681;
+    }
+
+    #line-copy{
+        border: none;
+        border-left: 1px solid hsla(200, 10%, 50%,100);
+        height: 40px;
+        width: 1px;
+        background-color: #efefef;
+        opacity: 20%;
+    }
+
+    .nav-item{
+        padding-right: 12px;
+        margin-top: 12px;
+        
+    }
+    .img{
+        padding-left: 12px;
+    }
+    .dropdown-item{
+        color: #555555;
+        font-size: 12px;
+        font-weight: 500;
+        font-style: normal;
+        letter-spacing: normal;
+        line-height: normal;
+        text-align: left;
+        margin-top: 22px;
+    }
+
+    #abelardo-acebado{
+        width: 138px;
+        height: 17px;
+        color: #273f70;
+        font-size: 16px;
+        font-weight: 600;
+        font-style: normal;
+        letter-spacing: normal;
+        line-height: normal;
+        text-align: left;
+        margin-top: 10px;
+        margin-bottom: 25px;
+    }
+
+    .dropdown-divider{
+        width: 134px;
+        height: 1px;
+        margin-left: 25px;
+        opacity: 90%;
+    }
 </style>
